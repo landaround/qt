e4d421e7a9011c100ff762a3e5ab917c1c38a9d0
land_around <y.bokitko@gmail.com>
2024-02-15 20:31:33 +0100

    hw42

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QMessageBox>
+#include <QRandomGenerator>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,12 +22,31 @@ public:
 
 private slots:
     void on_pushButton_clicked();
-
-    void on_pushButton2_clicked();
-
-    void on_pushButton3_clicked();
+    void on_pushButton_1_clicked();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
+    void on_pushButton_5_clicked();
+    void on_pushButton_6_clicked();
+    void on_pushButton_7_clicked();
+    void on_pushButton_8_clicked();
+    void on_pushButton_9_clicked();
 
 private:
     Ui::MainWindow *ui;
+    QChar board[3][3];
+    const QChar X='X';
+    const QChar O='O';
+    const QChar _=' ';
+    QChar human, comp;
+    bool gameover;
+    int counter;
+    QPushButton* buttons[3][3];
+    void display();
+    void move_human(QChar);
+    void move_comp(QChar);
+    void game_play();
+    void choose();
+    int check_win();
 };
 #endif // MAINWINDOW_H
